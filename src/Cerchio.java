65903ba1504228d84f6df6c82c9eadb63861c9d3
carloz1310 <carlozecca1@gmail.com>
2023-11-16 14:29:50 +0100

    first commit

---
+++ src/Cerchio.java
@@ -0,0 +1,36 @@
+public class Cerchio extends Forma {
+
+    private double raggio;
+    
+    public Cerchio(double raggio) {
+        this.raggio = raggio;
+    }
+
+    @Override
+    public double area() {
+        return raggio * raggio * Math.PI;
+    }
+
+    public double getRaggio() {
+        return raggio;
+    }
+
+    @Override
+    public String toString() {
+        return "Cerchio [raggio=" + raggio + "]";
+    }
+
+    @Override
+    public double perimetro() {
+        return extracted();
+    }
+
+    private double extracted() {
+        return 2*raggio*Math.PI;
+    }
+
+    public void setRaggio(double raggio) {
+        this.raggio = raggio;
+    }
+
+}
